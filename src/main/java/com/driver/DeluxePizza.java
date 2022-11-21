6a3c46b4d2054addcbf5a60be4ed98cf38b08421
rathin <rathin2104@gmail.com>
2022-11-21 11:37:13 +0530

    Add pizza Billing Code

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,13 @@
 package com.driver;
 
-public class DeluxePizza extends Pizza {
+class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        super.price+=80;
+        if(isVeg==true)
+            super.price+=70;
+        else
+            super.price+=120;
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,75 @@
 package com.driver;
 
-public class Pizza {
+class Pizza {
 
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean extraCheese=false;
+    private boolean extraToppings=false;
+    private boolean paperBag = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg==true)
+            price=300;
+        else
+            price=400;
         // your code goes here
     }
 
     public int getPrice(){
+        
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(extraCheese==true) 
+            return;
+        extraCheese=true;
+        price +=80;
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(extraToppings==true) 
+            return;
+        extraToppings=true;
+        if(isVeg==true)
+            price +=70;
+        else 
+            price +=120;
     }
 
     public void addTakeaway(){
         // your code goes here
+        paperBag=true;
+        price +=40;
     }
 
     public String getBill(){
         // your code goes here
+        if(isVeg==true)
+            bill +="Base Price Of The Pizza: 300"+'\n';
+        else
+            bill +="Base Price Of The Pizza: 400"+'\n';
+        
+        if(extraCheese==true)
+            bill +="Extra Cheese Added: 80"+'\n';
+
+        if(extraToppings==true){
+            if(isVeg==true)
+                bill+="Extra Toppings Added: 70"+'\n';
+            else
+                bill+="Extra Toppings Added: 120"+'\n';
+        }
+
+        if(paperBag==true)
+            bill +="Paperbag Added: 20"+'\n';
+
+        bill +="Total Price:"+price;
         return this.bill;
     }
 }
